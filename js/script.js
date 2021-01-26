2e82a62c95d478dd82bc9c57d565eac367e58651
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-01-26 16:41:11 +0000

    udated version

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-let subject = document.getElementById('subjectType');
-let quantity_of_questions = document.getElementById('amoutOfQuestions');
-let url = 'https://kit-questions.glitch.me/question/' + subject.value + "/" + quantity_of_questions.value
-console.log(url);
-let start_quiz_btn = document.getElementById('startBtn');
-
 function beginTeaser(){
+    //Targeting HTML Elements
+    let subject = document.getElementById('subjectType').value;
+
+    let quantity_of_questions = document.getElementById('amoutOfQuestions').value;
+
+    let url = 'https://kit-questions.glitch.me/question/' + subject + "/" + quantity_of_questions;
+
+    let start_quiz_btn = document.getElementById('startBtn');
+
+    //calling the API
     fetch(url)
     .then(Response=>Response.json())
     .then(data=>console.log(data))
     .catch((error)=>{
         console.log('error');
-    })
+    });
+
+    console.log(quantity_of_questions);
+    console.log(subject);
+
 }
+
+start_quiz_btn.addEventListener("click", beginTeaser());
 
 
 
