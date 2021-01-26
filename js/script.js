6e79eba6a93d14949a3ba1c8d511d0a84cdf83b6
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-01-26 13:33:49 +0000

    functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-fetch('https://kit-questions.glitch.me/question/english/4')
-.then(Response=>Response.json())
-.then(data=>console.log(data))
-// .catch((error)=>{
-//     console.log('error');
-// })
+let subject = document.getElementById('subjectType');
+let quantity_of_questions = document.getElementById('amoutOfQuestions');
+let url = 'https://kit-questions.glitch.me/question/' + subject.value + "/" + quantity_of_questions.value
+console.log(url);
+let start_quiz_btn = document.getElementById('startBtn');
+
+function beginTeaser(){
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(data=>console.log(data))
+    .catch((error)=>{
+        console.log('error');
+    })
+}
+
+
+
